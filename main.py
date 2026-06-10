87f0356d23943368a100dee135e72b6c3f29cb18
Muhammad Ameen <sheen93@users.noreply.github.com>
2026-06-10 12:01:48 -0400

    Update main.py with production ready code

--- main.py
+++ main.py
@@ -12,27 +12,37 @@ import random
 import smtplib
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+my_email = os.environ.get("MY_EMAIL")
+password = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+current_dt = dt.datetime.now()
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+df = pandas.read_csv("birthdays.csv")
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+# Production Ready Way:
+today_bdays = df[(df["month"] == current_dt.month) & (df["day"] == current_dt.day)]
+if not today_bdays.empty:
+    try:
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+
+            for index, bday_person in today_bdays.iterrows():
+                name = bday_person["name"]
+                target_mail = bday_person["email"]
+
+                template_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+                with open(template_path) as letter_file:
+                    letter_content = letter_file.read()
+                    letter_content = letter_content.replace("[NAME]", name)
+
+                msg = EmailMessage()
+                msg["Subject"] = "HAPPY BIRTHDAY"
+                msg["From"] = my_email
+                msg["To"] = target_mail
+                msg.set_content(letter_content)
+
+                connection.send_message(msg)
+                print(f"Birthday email sent to {name} at {target_mail}")
+    except Exception as e:
+        print(f"Network or SMTP error occurred: {e}")
